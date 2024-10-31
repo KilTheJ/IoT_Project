5a04dc16ee47a06b655b10fd4259a027ed31dde7
KilTheJ <jolivet.kilian@gmail.com>
2024-10-31 09:34:27 +0100

    Mise en place des threads pour l'adc, le capteur d'humidité et température

--- src/main.c
+++ src/main.c
@@ -17,9 +17,20 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 
+
+
+
+
+
+
+
+
+#define K_THREAD_DEFINE(name, stack_size, entry, p1, p2, p3, prio, options, delay )
+
 #define LED_YELLOW_NODE DT_ALIAS(led_yellow)
 #define LCD_I2C_NODE DT_ALIAS(led_screen)
-
+#define BUTTON_0_NODE DT_ALIAS(button_0)
+#define BUTTON_1_NODE DT_ALIAS(button_1)
 
 #include <zephyr/sys/util.h>
 
@@ -41,35 +52,11 @@ static const struct adc_dt_spec adc_channels[] = {
 const struct gpio_dt_spec led_yellow_gpio = GPIO_DT_SPEC_GET_OR(LED_YELLOW_NODE, gpios, {0});
 const struct i2c_dt_spec dev_lcd_screen = I2C_DT_SPEC_GET(LCD_I2C_NODE);
 const struct device *const dht11 = DEVICE_DT_GET_ONE(aosong_dht);
+const struct gpio_dt_spec button_0 = GPIO_DT_SPEC_GET_OR(BUTTON_0_NODE, gpios, {0});
+const struct gpio_dt_spec button_1 = GPIO_DT_SPEC_GET_OR(BUTTON_1_NODE, gpios, {0});
 
-int main(void) {
-  // Init device
-    init_lcd(&dev_lcd_screen);
-
-    // Display a message
-    write_lcd(&dev_lcd_screen, HELLO_MSG, LCD_LINE_1);
-    //write_lcd_clear(&dev_lcd_screen, ZEPHYR_MSG, LCD_LINE_2);
-    gpio_pin_configure_dt(&led_yellow_gpio, GPIO_OUTPUT_HIGH);
-
-    struct sensor_value temperature, humidity;
-    // Display temperature and humidity level
-    
-    
-
-    
-
-    while (1) { 
-        // Température et humidité :
-        sensor_sample_fetch(dht11);
-        sensor_channel_get(dht11, SENSOR_CHAN_AMBIENT_TEMP, &temperature);
-        sensor_channel_get(dht11, SENSOR_CHAN_HUMIDITY, &humidity);
-        int temp = sensor_value_to_double(&temperature);
-        int hum = sensor_value_to_double(&humidity);
-        printf("La temperature est de %d°C \n", temperature);
-        printf("Le taux d'humidité est de %d pourcents \n", humidity);
-        k_sleep(K_SECONDS(5)); 
-
-        // ADC :
+void thread_humidity_ADC(){
+    // ADC :
         int err;
 	    uint32_t count = 0;
         uint16_t buf;
@@ -131,8 +118,46 @@ int main(void) {
                 }
             }
         }
-        return 0;
+  
+
+void thread_humidity_and_temperature(){
+    struct sensor_value temperature, humidity;
+    // Display temperature and humidity level
+
+    while (1) { 
+        // Température et humidité :
+        sensor_sample_fetch(dht11);
+        sensor_channel_get(dht11, SENSOR_CHAN_AMBIENT_TEMP, &temperature);
+        sensor_channel_get(dht11, SENSOR_CHAN_HUMIDITY, &humidity);
+        int temp = sensor_value_to_double(&temperature);
+        int hum = sensor_value_to_double(&humidity);
+        printf("La temperature est de %d°C \n", temperature);
+        printf("Le taux d'humidité est de %d pourcents \n", humidity);
+        
 
         
+        return 0;
+
+    }
+
 }
 
+
+int main(void) {
+    // Init device
+    init_lcd(&dev_lcd_screen);
+
+    // Display a message
+    write_lcd(&dev_lcd_screen, HELLO_MSG, LCD_LINE_1);
+    //write_lcd_clear(&dev_lcd_screen, ZEPHYR_MSG, LCD_LINE_2);
+    gpio_pin_configure_dt(&led_yellow_gpio, GPIO_OUTPUT_HIGH);
+    while(1){
+    // appel du thread humidity and temperature :
+    thread_humidity_and_temperature();
+    //appel du thread ADC:
+    thread_humidity_ADC();  
+    k_sleep(K_SECONDS(5)); 
+    }
+}
+K_THREAD_DEFINE(thread_id_ADC, 521, thread_humidity_ADC, NULL, NULL, NULL, 9, 0, 0)
+K_THREAD_DEFINE(thread_id_HT, 521, thread_humidity_and_temperature, NULL, NULL, NULL, 9, 0, 0)
